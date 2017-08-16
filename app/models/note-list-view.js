af48cb9411d5a9abb35f1bf54d988917d7e40247
Phil Lang <plangphoto@gmail.com>
2017-08-16 09:49:35 +0100

    Mapping instead for forEach on array

--- app/models/note-list-view.js
+++ app/models/note-list-view.js
@@ -5,7 +5,7 @@
 
   View.prototype.printHtml = function() {
     let html = "<ul>";
-    this._list._notes.forEach( function(note) {
+    this._list._notes.map( function(note) {
       html += "<li>" + note._text + "</li>";
     });
     return html += "</ul>";
